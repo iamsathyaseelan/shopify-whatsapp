93473f467d7717b3936f9fb455caa9a5696a76f7
Sathyaseelan <iamsathyaseelan@gmail.com>
2021-01-19 12:58:10 +0530

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,4 +1,5 @@
 "use strict";
+
 function whatsappChat() {
     let isMobile = function () {
         let check = false;
@@ -34,10 +35,17 @@ function whatsappChat() {
         document.getElementsByTagName("head")[0].appendChild(style);
     }
     let initChat = function () {
-        let chat = generateChatIcon();
+        let label = getQueryParam('label', currentScript.src);
         let phone = getQueryParam('phone', currentScript.src);
         let position = getQueryParam('position', currentScript.src);
-        chat.setAttribute('class', position);
+        let className = position;
+        let iconText = '';
+        if (label && label.length > 1) {
+            className += ' hasText';
+            iconText += `<div class="iconText"><p>${label}</p></div>`;
+        }
+        let chat = generateChatIcon(iconText);
+        chat.setAttribute('class', className);
         let link = `https://web.whatsapp.com/send`;
         if (isMobile()) {
             link = `https://api.whatsapp.com/send`;
@@ -46,27 +54,25 @@ function whatsappChat() {
             window.open(`${link}/?phone=${phone}`, "_blank");
         }
     }
-    let generateChatIcon = function () {
+    let generateChatIcon = function (iconText) {
         const whatsappChat = document.createElement('div');
         whatsappChat.setAttribute('id', 'whatsapp-chat-widget-container');
-        whatsappChat.innerHTML = '<div id="whatsapp-chat-widget-icon"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path d="M12.031 6.172c-3.181 0-5.767 2.586-5.768 5.766-.001 1.298.38 2.27 1.019 3.287l-.582 2.128 2.182-.573c.978.58 1.911.928 3.145.929 3.178 0 5.767-2.587 5.768-5.766.001-3.187-2.575-5.77-5.764-5.771zm3.392 8.244c-.144.405-.837.774-1.17.824-.299.045-.677.063-1.092-.069-.252-.08-.575-.187-.988-.365-1.739-.751-2.874-2.502-2.961-2.617-.087-.116-.708-.94-.708-1.793s.448-1.273.607-1.446c.159-.173.346-.217.462-.217l.332.006c.106.005.249-.04.39.298.144.347.491 1.2.534 1.287.043.087.072.188.014.304-.058.116-.087.188-.173.289l-.26.304c-.087.086-.177.18-.076.354.101.174.449.741.964 1.201.662.591 1.221.774 1.394.86s.274.072.376-.043c.101-.116.433-.506.549-.68.116-.173.231-.145.39-.087s1.011.477 1.184.564.289.13.332.202c.045.072.045.419-.1.824zm-3.423-14.416c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.029 18.88c-1.161 0-2.305-.292-3.318-.844l-3.677.964.984-3.595c-.607-1.052-.927-2.246-.926-3.468.001-3.825 3.113-6.937 6.937-6.937 1.856.001 3.598.723 4.907 2.034 1.31 1.311 2.031 3.054 2.03 4.908-.001 3.825-3.113 6.938-6.937 6.938z"/></svg></div>';
+        whatsappChat.innerHTML = `<div id="whatsapp-chat-widget-icon"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path d="M12.031 6.172c-3.181 0-5.767 2.586-5.768 5.766-.001 1.298.38 2.27 1.019 3.287l-.582 2.128 2.182-.573c.978.58 1.911.928 3.145.929 3.178 0 5.767-2.587 5.768-5.766.001-3.187-2.575-5.77-5.764-5.771zm3.392 8.244c-.144.405-.837.774-1.17.824-.299.045-.677.063-1.092-.069-.252-.08-.575-.187-.988-.365-1.739-.751-2.874-2.502-2.961-2.617-.087-.116-.708-.94-.708-1.793s.448-1.273.607-1.446c.159-.173.346-.217.462-.217l.332.006c.106.005.249-.04.39.298.144.347.491 1.2.534 1.287.043.087.072.188.014.304-.058.116-.087.188-.173.289l-.26.304c-.087.086-.177.18-.076.354.101.174.449.741.964 1.201.662.591 1.221.774 1.394.86s.274.072.376-.043c.101-.116.433-.506.549-.68.116-.173.231-.145.39-.087s1.011.477 1.184.564.289.13.332.202c.045.072.045.419-.1.824zm-3.423-14.416c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.029 18.88c-1.161 0-2.305-.292-3.318-.844l-3.677.964.984-3.595c-.607-1.052-.927-2.246-.926-3.468.001-3.825 3.113-6.937 6.937-6.937 1.856.001 3.598.723 4.907 2.034 1.31 1.311 2.031 3.054 2.03 4.908-.001 3.825-3.113 6.938-6.937 6.938z"/></svg></div>${iconText}`;
         document.body.append(whatsappChat);
         injectCss(`
-        #whatsapp-chat-widget-container{
-        max-width:120px;
-        position:fixed;
-        z-index:9999999;
-        cursor:pointer;
-        }#whatsapp-chat-widget-icon svg{fill:#22cd5b;background: white;border-radius: 50%;}
-        #whatsapp-chat-widget-container.bottomLeft{bottom:20px;left:20px;}
-        #whatsapp-chat-widget-container.bottomRight{bottom:20px;right:20px;}
-        #whatsapp-chat-widget-container.centeredRight{bottom:48%;right:20px;}
-        #whatsapp-chat-widget-container.centeredLeft{bottom:48%;left:20px;}
+        #whatsapp-chat-widget-container{max-width:100%;position:fixed;z-index:9999999;cursor:pointer;}
+        #whatsapp-chat-widget-icon svg{fill:#22cd5b;background: white;border-radius: 50%;}
+        #whatsapp-chat-widget-container.bottomLeft{bottom:20px;left:10px;}
+        #whatsapp-chat-widget-container.bottomRight{bottom:20px;right:10px;}
+        #whatsapp-chat-widget-container.centeredRight{bottom:48%;right:10px;}
+        #whatsapp-chat-widget-container.centeredLeft{bottom:48%;left:10px;}
+        #whatsapp-chat-widget-container.hasText{background-color:#22cd5b;display:flex;border-radius:5px}
+        #whatsapp-chat-widget-container .iconText p{color:#ffffff;margin: 10px 10px 10px 0;}
+        #whatsapp-chat-widget-container.hasText svg{width: 30px;height: 30px;margin: 8px;}
         `);
         return whatsappChat;
     }
-    document.addEventListener('DOMContentLoaded', (event) => {
-    });
     return initChat();
 }
+
 window.whatsappChat = whatsappChat();
